7b15de27324df23e801d0d9c9b05679115eb9558
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-02-20 19:57:24 -0400

    main core

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import Question from "./components/Question";
+
+import "./App.css";
+
+const questions = [
+  {
+    id: 1,
+    title: "Is this a good product?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+  },
+  {
+    id: 2,
+    title: "How much does it cost?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+  },
+  {
+    id: 3,
+    title: "When can I get it?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div >
-      klklk
+    <div>
+      <h1>FAQ section</h1>
+      <div className="container">
+        <h2>Frequently Asked questions</h2>
+        <div className="questions">
+          {questions.map((question) => (
+            <Question key={question.id} question={question} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
