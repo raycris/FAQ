b4703924779ad55d5fd2ffd80c97d715c82e0373
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-02-24 17:24:05 -0400

    lenguage complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import { createContext, useState } from "react";
 
 import Question from "./components/Question";
-
 import Header from "./components/Header";
 
 import "./App.css";
 
-const questions = [
-  {
-    id: 1,
-    title: "Is this a good product?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
-  },
-  {
-    id: 2,
-    title: "How much does it cost?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
-  },
-  {
-    id: 3,
-    title: "When can I get it?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
-  },
-];
+import { useTranslation } from "react-i18next";
 
 export const ThemeContext = createContext(null);
 
@@ -31,14 +14,51 @@ function App() {
   const toggleTheme = () => {
     setTheme((current) => (current === "light" ? "dark" : "light"));
   };
-  
+  const [t, i18n] = useTranslation("global");
+
+  const questions = [
+    {
+      id: 1,
+      title: (
+        <>
+          {i18n.language === "en"
+            ? "Is this a good product?"
+            : "¿Es un buen producto?"}
+        </>
+      ),
+      info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+    },
+    {
+      id: 2,
+      title: (
+        <>
+          {i18n.language === "en"
+            ? "How much does it cost?"
+            : "¿Cuánto cuesta?"}
+        </>
+      ),
+      info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+    },
+    {
+      id: 3,
+      title: (
+        <>
+          {i18n.language === "en"
+            ? "When can I get it?"
+            : "¿Cuándo puedo conseguirlo?"}
+        </>
+      ),
+      info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+    },
+  ];
+
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Header theme={theme} toggleTheme={toggleTheme} />
-        <h1>FAQ section</h1>
+        <h1>{t("faq_section")}</h1>
         <div className="container">
-          <h2>Frequently Asked questions</h2>
+          <h2>{t("faq")}</h2>
           <div className="questions">
             {questions.map((question) => (
               <Question key={question.id} question={question} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,29 @@
 import Switch from "react-switch";
 
+import { useTranslation } from "react-i18next";
+
 import "./Header.css";
 
 const Header = (props: { toggleTheme: any; theme: string }) => {
+  const [t, i18n] = useTranslation("global");
+
   return (
     <header className="headerContainer">
       <div className="switchConatiner">
-        <div></div>
+        <div>
+          <button onClick={() => i18n.changeLanguage("es")}>ES</button>
+          <button onClick={() => i18n.changeLanguage("en")}>EN</button>
+        </div>
         <div className="infoContainer">
-          <label>{props.theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          {i18n.language === "en" ? (
+            <label>
+              {props.theme === "light" ? "Light Mode" : "Dark Mode"}
+            </label>
+          ) : (
+            <label>
+              {props.theme === "light" ? "Modo Claro" : "Modo Oscuro"}
+            </label>
+          )}
           <Switch
             onChange={props.toggleTheme}
             checked={props.theme === "dark"}
